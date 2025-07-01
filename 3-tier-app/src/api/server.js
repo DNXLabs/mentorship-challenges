53f2af960c16f5f305a58dc14675dcd28350229c
Thiago Shimada Ramos <thiago.shimada@dnx.solutions>
2025-07-01 18:17:03 +1000

    change package to mysql2

--- 3-tier-app/src/api/server.js
+++ 3-tier-app/src/api/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { v4: uuidv4 } = require('uuid');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const app = express();
 const port = process.env.PORT || 3000;
 
